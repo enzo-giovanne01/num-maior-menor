9fb872c65266d5401ade3cfa1d15b86fbe1eae08
enzo-giovanne01 <giovanneenzo01@gmail.com>
2024-09-02 00:02:59 -0300

    num-maior-menor

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+int a, b;
+WriteLine("Digite o 1º valor:");
+a = Convert.ToInt16(ReadLine());
+WriteLine("Digite o 2º valor");
+b = Convert.ToInt16(ReadLine());
+int soma = a + b;
+if(soma >= 100)
+{   
+    WriteLine("A soma foi maior que 100, o valor foi: "+soma);
+}
+else
+{
+    WriteLine("A soma foi menor que 100, o valor foi: "+ soma);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# num-maior-menor
